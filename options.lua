66619b62a4ba2480191891c1eba0532050dd231b
Aidan Border <planes4u@outlook.com>
2024-01-02 11:55:31 -0500

    Making cmdheight actually work
    Pro tip: cmdheight must be specified in the opts struct thingy, not the g struct thingy

--- options.lua
+++ options.lua
@@ -7,6 +7,7 @@ return {
     spell = false, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
+    cmdheight = 1,
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
@@ -17,7 +18,6 @@ return {
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
     resession_enabled = false, -- enable experimental resession.nvim session management (will be default in AstroNvim v4) 
-    cmdheight = 1,
   },
 }
 -- If you need more control, you can use the function()...end notation
