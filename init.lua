3b2def9ca5d2fa434ac54e631172ce065faf1b8b
Aidan Border <planes4u@outlook.com>
2024-01-11 23:49:47 -0500

    Just kidding I actually like Nord better

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "gruvbox",
+  colorscheme = "nord",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
