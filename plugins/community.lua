dab12113aae9f08ed0f661cc06400188390a2c87
Aidan Border <planes4u@outlook.com>
2024-01-11 23:47:04 -0500

    Adding Gruvbox plugin and changing colorscheme to Gruvbox

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,7 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
   { import = "astrocommunity.bars-and-lines.lualine-nvim" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.java" },
